d1db93db1cf95d460638834d94f3bf561490f586
depth81 <petv1981@gmail.com>
2020-09-29 19:02:00 -0500

    xxx

---
+++ OOP/Ejemplo3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ejemplo3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
